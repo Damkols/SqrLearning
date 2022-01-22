7cfb5d41d40b45f10aeaf69ba5e01c4a14155cfc
damkols <damolausmankolapo@gmail.com>
2022-01-22 10:51:17 -0800

    Built Navbar for Sqrleaning

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React, { useContext } from "react";
-import { ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme } from "./styles/theme";
-import { GlobalStyle } from "./styles/global";
-import { ThemeContext } from "./context/themeContext";
-import DashBoard from "./containers/DashBoard";
+import React from "react";
+import NavBar from "./components/NavBar";
 
 const App = () => {
-  const context = useContext(ThemeContext);
-  const { theme } = context;
-
   return (
-    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-      <>
-        <GlobalStyle />
-        <DashBoard />
-      </>
-    </ThemeProvider>
+    <div>
+      <NavBar />
+    </div>
   );
 };
 
